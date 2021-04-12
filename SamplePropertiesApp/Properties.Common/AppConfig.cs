dbb4aa428c7b404700f1908b4487c07bdcccff6b
Tarun Kumar Ghorui <ghorui.tarun@gmail.com>
2021-04-12 11:11:48 +0530

    Add files via upload

---
+++ SamplePropertiesApp/Properties.Common/AppConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Properties.Common
+{
+    public class AppConfig
+    {
+        private static IConfiguration Configuration;
+        static AppConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        public static IConfigurationSection Get(string name)
+        {
+            IConfigurationSection appSettings = Configuration.GetSection(name);
+            return appSettings;
+        }
+    }
+}
