60c2835cc7d654eeb27432c644c14fae708dafc3
Tarun Kumar Ghorui <ghorui.tarun@gmail.com>
2021-04-12 11:12:51 +0530

    Add files via upload

---
+++ SamplePropertiesApp/Properties.Repository/InteractionGetRepository.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using Properties.Common;
+using Properties.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+
+namespace Properties.Repository
+{
+    public interface IInteractionGetRepository
+    {
+        public List<PropertyInfo> GetResponse();
+    }
+
+    public class InteractionGetRepository : IInteractionGetRepository
+    {
+        private readonly HttpClient _httpClient;
+        private string connectionString = AppConfig.Get("Config")["ConnectionString"];
+
+        public InteractionGetRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public InteractionGetRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public List<PropertyInfo> GetResponse()
+        {
+            var url = AppConfig.Get("Config")["SourceUrl"];
+            var response = _httpClient.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<PropertiesResponseDto>(result.Result);
+                var ExistingList = GetExistingPropertyIds();
+                return data.GetRequiredProperties(ExistingList);
+            }
+
+            return null;
+        }
+
+        private List<int> GetExistingPropertyIds()
+        {
+            List<int> response = new List<int>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var cmdText = "usp_GetPropertyIds";
+                    SqlCommand cmd = new SqlCommand(cmdText, connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            int.TryParse(dataReader[0].ToString(), out var id);
+                            response.Add(id);
+                        };
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ SamplePropertiesApp/Properties.Repository/InteractionPostRepository.cs
@@ -0,0 +1,49 @@
+﻿using Properties.Common;
+using Properties.Model;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Properties.Repository
+{
+    public interface IInteractionPostRepository
+    {
+        bool SaveDataToDb(PropertyInfo input);
+    }
+
+    public class InteractionPostRepository : IInteractionPostRepository
+    {
+        private string connectionString =  AppConfig.Get("Config")["ConnectionString"];
+        public bool SaveDataToDb(PropertyInfo input)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var cmdText = "usp_saveProperty";
+                    SqlCommand cmd = new SqlCommand(cmdText, connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", input.Id);
+                    cmd.Parameters.AddWithValue("@year_built", input.YearBuilt);
+                    cmd.Parameters.AddWithValue("@list_price", input.ListPrice);
+                    cmd.Parameters.AddWithValue("@monthly_rent", input.MonthlyRent);
+                    cmd.Parameters.AddWithValue("@gross_yield", input.GrossYield);
+                    cmd.Parameters.AddWithValue("@address1", input.Address.Address1);
+                    cmd.Parameters.AddWithValue("@address2", input.Address.Address2);
+                    cmd.Parameters.AddWithValue("@city", input.Address.City);
+                    cmd.Parameters.AddWithValue("@country", input.Address.Country);
+                    cmd.Parameters.AddWithValue("@district", input.Address.District);
+                    cmd.Parameters.AddWithValue("@state", input.Address.State);
+                    cmd.Parameters.AddWithValue("@zip", input.Address.Zip);
+                    cmd.Parameters.AddWithValue("@zip_plus4", input.Address.ZipPlus4);
+
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
