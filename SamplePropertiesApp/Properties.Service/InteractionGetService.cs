b6eca0df28742747b024744c98bdb8a8ae29944d
Tarun Kumar Ghorui <ghorui.tarun@gmail.com>
2021-04-12 11:13:21 +0530

    Add files via upload

---
+++ SamplePropertiesApp/Properties.Service/InteractionGetService.cs
@@ -0,0 +1,28 @@
+﻿using Properties.Common;
+using Properties.Model;
+using Properties.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Properties.Service
+{
+    public interface IInteractionGetService 
+    {
+        public Task<List<PropertyInfo>> GetResponseAsync();
+    }
+
+    public class InteractionGetService : IInteractionGetService
+    {
+        private readonly IInteractionGetRepository _interactionRepository;
+
+        public InteractionGetService(IInteractionGetRepository interactionRepository)
+        {
+            _interactionRepository = interactionRepository;
+        }
+
+         public async Task<List<PropertyInfo>> GetResponseAsync()
+        {
+            return _interactionRepository.GetResponse();
+        }
+    }
+}
---
+++ SamplePropertiesApp/Properties.Service/InteractionPostService.cs
@@ -0,0 +1,32 @@
+﻿using Properties.Common;
+using Properties.Model;
+using Properties.Repository;
+using System.Threading.Tasks;
+
+namespace Properties.Service
+{
+    public interface IInteractionPostService 
+    {
+        public Task<bool> GetResponseAsync(PropertyInfo input);
+    }
+
+    public class InteractionPostService : IInteractionPostService
+    {
+        private readonly IInteractionPostRepository _interactionPostRepository;
+
+        public InteractionPostService(IInteractionPostRepository interactionPostRepository)
+        {
+            _interactionPostRepository = interactionPostRepository;
+        }
+
+        public async Task<bool> GetResponseAsync(PropertyInfo input)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+
+            return _interactionPostRepository.SaveDataToDb(input);
+        }
+    }
+}
